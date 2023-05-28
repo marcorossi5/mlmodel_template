ca7990f40fe3fe4b7821bae3b0880b8b7bf8d8d1
Marco Rossi <marco.rossi@digitiamo.com>
2023-05-28 15:10:22 +0200

    WIP integrate mlflow in optuna

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import logging
+import os
 from typing import Dict, Any
 
 import mlflow
 import optuna
+from optuna.integration.mlflow import MLflowCallback
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_diabetes
@@ -11,6 +13,13 @@ from sklearn.ensemble import RandomForestRegressor
 from .logger import logger
 from .metrics import compute_metrics
 from .utils import set_seed
+
+
+mlflc = MLflowCallback(
+        tracking_uri=os.environ.get("MLFLOW_TRACKING_URI"),
+        metric_name="my metric score",
+        create_experiment=True
+    )
 
 
 def load_data():
@@ -41,39 +50,53 @@ def train_model(model, data, hparams):
 
 
 def objective(trial, data):
-    with mlflow.start_run():
-        max_depth = trial.suggest_int('rf_max_depth', 2, 32, log=True)
-        logger.info("Set seed")
-        seed = set_seed()
+    max_depth = trial.suggest_int('rf_max_depth', 2, 32, log=True)
+    logger.info("Set seed")
+    seed = set_seed()
 
-        hparams = {"n_estimators": 100, "max_depth": max_depth, "seed": seed}
-        mlflow.log_params(hparams)
+    hparams = {"n_estimators": 100, "max_depth": max_depth, "seed": seed}
+    mlflow.log_params(hparams)
 
-        logger.info("Load model")
-        model = load_model(hparams)
+    logger.info("Load model")
+    model = load_model(hparams)
 
-        logger.info("Train model")
-        model = train_model(model, data, hparams)
+    logger.info("Train model")
+    model = train_model(model, data, hparams)
 
-        logger.info("Predict")
-        y_preds = model.predict(data["test"])
-        metrics = compute_metrics(y_preds, data["test_labels"])
-        return metrics["rmse"]
+    logger.info("Predict")
+    y_preds = model.predict(data["test"])
+    metrics = compute_metrics(y_preds, data["test_labels"])
+    return metrics["rmse"]
+
+
+def get_experiment_from_name(mlflow_client, experiment_name):
+    experiment = mlflow_client.get_experiment_by_name(experiment_name)
+    if experiment is None:
+        # experiment does not exist, create it
+        logger.info("Create brand new experiment named: %s", experiment_name)
+        experiment_id = mlflow_client.create_experiment(experiment_name)
+        return mlflow_client.get_experiment(experiment_id)
+    return experiment
 
 
 def main():
     logger.info("Load dataset")
     data = load_data()
+    
+    experiment_name = os.environ.get("EXPERIMENT_NAME", "example_1")
+    
+    @mlflc.track_in_mlflow()
+    def func(trial):
+        return objective(trial, data)
 
-    func = lambda trial: objective(trial, data)
-
-    n_trials = 100
+    n_trials = 20
     logger.info("Hyperparameter search for %d trials", n_trials)
     
     original_level = logger.getEffectiveLevel()
     logger.setLevel(logging.WARNING)
-    study = optuna.create_study(direction='minimize')
-    study.optimize(func, n_trials=n_trials)
+    study = optuna.create_study(direction='minimize', study_name=experiment_name)
+
+    study.optimize(func, n_trials=n_trials, callbacks=[mlflc])
     logger.setLevel(original_level)
 
 
